439961c44fdd74272023b0f758ca4ac20bfe3ad7
LeoOptifi <chun.w@alphabit.software>
2023-02-10 15:34:02 +0800

    final version

--- README.md
+++ README.md
@@ -1,34 +1,37 @@
 # chialisp-mojodice
 
-
-
 # Example
 
 receiver: 0x574cfa983d0e24354484db66a68a2e5e11711bf74192056a256eeee09576c968
 
 存錢罐(created after curry treehash): b9a3a2f9b33d71c9775fb9600a10f7ea39fab91962d09a7302c7bb6c7ab1bbe5
 標準地址:txch1hx3697dn84cuja6lh9sq5y8hagul4wgevtgf5uczc7akc743h0jsszcwpk
-錢包public key:0xb6d62517192be93313f6191249af1b139445b4842fa4c7930b9f3fe824661500db594ab76eb9c8ed8caef3ed0f0f59a5
+錢包 public key:0xb6d62517192be93313f6191249af1b139445b4842fa4c7930b9f3fe824661500db594ab76eb9c8ed8caef3ed0f0f59a5
 (after curry )0719ee4a74d34581244133c878202735c80b878524cc5f870781107cd05876ba
 
-## create puzzle hash, set target amount 
-創建一個裡面有0元的存錢罐
-    cdv clsp build piggybank/piggybank.clsp
-    cdv clsp curry piggybank/piggybank.clsp.hex -a 2200 -a 0xeeda2dceabc6e970d30d2990177a9317d0301597c6ed552217d83aa07e1af107 --treehash
-    cdv encode b9a3a2f9b33d71c9775fb9600a10f7ea39fab91962d09a7302c7bb6c7ab1bbe5 --prefix txch
-    送錢給標準地址
-    <!-- chia wallet send -a 0 -t txch1s2y960mc8dfk6htjdgpwjylqhxwa077rt0ykh7gnlzgjc959klaqa87nwm -m 0.000006 --override -->
+## create puzzle hash, set target amount
+
+創建一個裡面有 0 元的存錢罐
+cdv clsp build piggybank/piggybank.clsp
+cdv clsp curry piggybank/piggybank.clsp.hex -a 2200 -a 0xeeda2dceabc6e970d30d2990177a9317d0301597c6ed552217d83aa07e1af107 --treehash
+cdv encode b9a3a2f9b33d71c9775fb9600a10f7ea39fab91962d09a7302c7bb6c7ab1bbe5 --prefix txch
+送錢給標準地址
+
+<!-- chia wallet send -a 0 -t txch1s2y960mc8dfk6htjdgpwjylqhxwa077rt0ykh7gnlzgjc959klaqa87nwm -m 0.000006 --override -->
 
 ## contributions spend (從自己的錢包分出一部分的錢 拿來存)
-從錢包劃分出一個有x元的存錢罐
-    cdv clsp build piggybank/contribution.clsp
-    cdv clsp curry piggybank/contribution.clsp.hex -a 0xb6d62517192be93313f6191249af1b139445b4842fa4c7930b9f3fe824661500db594ab76eb9c8ed8caef3ed0f0f59a5 --treehash
-    cdv encode e92932f02e9fbb6107074052f5757f7f69c27bd18ddd1a8bdc51ad770c7f7719 --prefix txch
-    送錢給地址(700)
-    chia wallet send -a 0.0000000007 -t txch1ay5n9upwn7akzpc8gpf02atl0a5uy7733hw34z7u2xkhwrrlwuvsqngtnr (--fee 0.000006)
+
+從錢包劃分出一個有 x 元的存錢罐
+cdv clsp build piggybank/contribution.clsp
+cdv clsp curry piggybank/contribution.clsp.hex -a 0xb6d62517192be93313f6191249af1b139445b4842fa4c7930b9f3fe824661500db594ab76eb9c8ed8caef3ed0f0f59a5 --treehash
+cdv encode e92932f02e9fbb6107074052f5757f7f69c27bd18ddd1a8bdc51ad770c7f7719 --prefix txch
+送錢給地址(700)
+chia wallet send -a 0.0000000007 -t txch1ay5n9upwn7akzpc8gpf02atl0a5uy7733hw34z7u2xkhwrrlwuvsqngtnr (--fee 0.000006)
+
 ## set spend bundle json
-  
+
 ### coin1
+
     cdv rpc coinrecords --by puzhash b9a3a2f9b33d71c9775fb9600a10f7ea39fab91962d09a7302c7bb6c7ab1bbe5 -nd
     puzzle_reveal:
     cdv clsp curry piggybank/piggybank.clsp.hex -a 3000 -a 0xeeda2dceabc6e970d30d2990177a9317d0301597c6ed552217d83aa07e1af107 -x
@@ -36,38 +39,46 @@ receiver: 0x574cfa983d0e24354484db66a68a2e5e11711bf74192056a256eeee09576c968
     opc '(200 900 0xa0a6659b200aef026393ff92f7fde6119a0fce985a68c4e5b7ea8bedd3d9afee)'
 
 ### coin2
+
     cdv rpc coinrecords --by puzhash e92932f02e9fbb6107074052f5757f7f69c27bd18ddd1a8bdc51ad770c7f7719 -nd
     puzzle_reveal:
     cdv clsp curry piggybank/contribution.clsp.hex -a 0xb6d62517192be93313f6191249af1b139445b4842fa4c7930b9f3fe824661500db594ab76eb9c8ed8caef3ed0f0f59a5 -x
     solution:(coin_id new_amount)
     opc '(0x8f540ee77a493b99ecee19b2ede43cbdc7854e83c2c166de6734ebae5b0e19ad 900)'
- 
+
 ### aggregated_signature
+
     python3 piggybank/sign_contribution.py
 
 ## send tx (wait half a hour)
-    cdv rpc pushtx spend_bundle.json 
+
+    cdv rpc pushtx spend_bundle.json
 
 # mojodice
 
 vault: eb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 (txch1avlhd02028tqraxjckrfpl720w5mfmr7ygkxj0k20q5lufsdputq8qalk9)
-劃分的coin:b40dcf127f4cfaf7f922ff53d49a08ffbf003e276fb1936fb1fc2fa95674b909
+劃分的 coin:b40dcf127f4cfaf7f922ff53d49a08ffbf003e276fb1936fb1fc2fa95674b909
 (txch1ksxu7ynlfna007fzlafafxsgl7lsq038d7cexma3lsh6j4n5hyysv4f07g)
+
 ## create puzzle hash, set target amount
+
     cdv clsp build piggybank/piggybank.clsp
     cdv clsp curry piggybank/piggybank.clsp.hex -a 0x574cfa983d0e24354484db66a68a2e5e11711bf74192056a256eeee09576c968 -a 40 -a 10000000  --treehash
     cdv encode eb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 --prefix txch
     送錢給標準地址
 
 ## contributions spend (從自己的錢包分出一部分的錢 拿來存)
+
     cdv clsp build piggybank/contribution.clsp
     cdv clsp curry piggybank/contribution.clsp.hex -a 0xb94a99bd56f43b5d4ab1a3eae5919ed29d897fdc662ba0cab8013a5a9d05d31ccd56c9b24bbd7cf9509f98c88ada438d --treehash
     cdv encode b40dcf127f4cfaf7f922ff53d49a08ffbf003e276fb1936fb1fc2fa95674b909 --prefix txch
-   送錢給標準地址
+
+送錢給標準地址
 
 ## set spend bundle json
-  
+
 ### coin1
+
     cdv rpc coinrecords --by puzhash eb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 -nd
     puzzle_reveal:
     cdv clsp curry piggybank/piggybank.clsp.hex -a 0x574cfa983d0e24354484db66a68a2e5e11711bf74192056a256eeee09576c968 -a 40 -a 10000000  -x
@@ -75,6 +86,7 @@ vault: eb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 (txch1av
        opc '(46 500 0xeb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 40 90 130)'
 
 ### coin2
+
     cdv rpc coinrecords --by puzhash b40dcf127f4cfaf7f922ff53d49a08ffbf003e276fb1936fb1fc2fa95674b909 -nd
       puzzle_reveal:
     cdv clsp curry piggybank/contribution.clsp.hex -a 0xb94a99bd56f43b5d4ab1a3eae5919ed29d897fdc662ba0cab8013a5a9d05d31ccd56c9b24bbd7cf9509f98c88ada438d -x
@@ -82,13 +94,17 @@ vault: eb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 (txch1av
       opc '(0xf5921e8a28057056e312359a1623c9956350188e1c568b055cde88f4587fc144 900)'
 
 ### aggregated_signature
+
     python3 piggybank/sign_contribution.py
 
-## send tx 
-    cdv rpc pushtx spend_bundle.json 
+## send tx
+
+    cdv rpc pushtx spend_bundle.json
 
 ## check
+
 ### withdraw
+
 1. receiver receive withdraw_amount
    cdv rpc coinrecords --by puzhash 574cfa983d0e24354484db66a68a2e5e11711bf74192056a256eeee09576c968
    has a coin with "spent_block_index": 0 / amount: withdraw_amount
@@ -96,37 +112,42 @@ vault: eb3f76bd4f51d601f4d2c58690ffca7ba9b4ec7e222c693eca7829fe260d0f16 (txch1av
    cdv rpc coinrecords --by puzhash 6a0326da0282f6d521f2f80d9b96c120fd5fd1bd7a20d49c75bf7490e07e345d
    has a coin with "spent_block_index": 0 / amount: vault_after_withdraw_amount
 
-### deposit
-
 # test code (driver)
+
 cdv test
-# other 
- ->(ERROR)One or more of the specified objects was not a spend bundle: bytes object is expected to start with 0x
- ->refer: https://docs.chia.net/guides/chialisp-first-smart-coin/ to add 0x
- -> -m 0.000006 --override 
- -> spent_block_index has value: means "spent true"
+
+# other
+
+->(ERROR)One or more of the specified objects was not a spend bundle: bytes object is expected to start with 0x
+->refer: https://docs.chia.net/guides/chialisp-first-smart-coin/ to add 0x
+-> -m 0.000006 --override
+-> spent_block_index has value: means "spent true"
 
 # note
-1. setup要用 pytest_asyncio, 不然會cannot unpack non-iterable async_generator object
-2. 不能用.as_coin(), 要用.coin
-3. 主要是合約問題！ 
-CREATE_COIN_ANNOUNCEMENT 拿掉會fail
-main 可以直接給() 一定會過
-4. test code:
-contribution_spend拿掉會MINTING_COIN fail
-contribution_spend amt 可以拿掉 ,args不能拿掉
 
+1. setup 要用 pytest_asyncio, 不然會 cannot unpack non-iterable async_generator object
+2. 不能用.as_coin(), 要用.coin
+3. 主要是合約問題
+   CREATE_COIN_ANNOUNCEMENT 拿掉會 fail
+   main 可以直接給() 一定會過
+4. test code:
+   contribution_spend 拿掉會 MINTING_COIN fail
+   contribution_spend amt 可以拿掉 ,args 不能拿掉
 
 TODO:
+
 - input: curry (o)
 - input: multi input (o)
 - contract: function without using (o)
 - contract: main func (o)
 - test withdraw: vault:100 user send: 40 withdraw_amount: 40 vault_after_withdraw_amount:100 (o)
 - test withdraw: vault:100 user send: 30 withdraw_amount: 40 vault_after_withdraw_amount:90 (o)
-- test deposit: vault:100 user send:40 vault_after_deposit_amount:140 
+- test deposit: vault:100 user send:40 vault_after_deposit_amount:140 (o)
 - success_number : hash by user name / secret key (o)
 - delete all comment (o)
-- drivers code ()
-- test code
-- recover deleted comment
+- drivers code (o)
+- test code (o)
+- use hash func (o)
+- recover deleted safety comment(o)
+- multiplier (o)
+- test wrong situation on python
--- tests/test_piggybank.py
+++ tests/test_piggybank.py
@@ -2,10 +2,13 @@ from typing import Dict, List, Optional
 
 import pytest
 import pytest_asyncio
+import hashlib
+import hmac
 from chia.types.blockchain_format.coin import Coin
 from chia.types.condition_opcodes import ConditionOpcode
 from chia.types.spend_bundle import SpendBundle
 from chia.util.ints import uint64
+from datetime import datetime
 
 from piggybank.piggybank_drivers import (
     create_piggybank_puzzle,
@@ -15,6 +18,30 @@ from piggybank.piggybank_drivers import (
 from cdv.test import CoinWrapper
 from cdv.test import setup as setup_test
 
+PROBABILITY = 50
+GAME_LIMIT = 10000000
+VAULT_BALANCE = 100
+WITHDRAW_MULTIPLIER = 2
+
+
+def gen_success_number(user_name, secret_key):
+    # encoding as per other answers
+    byte_key = bytes(secret_key, "UTF-8")  # key.encode() would also work in this case
+    message = user_name.encode()
+
+    # now use the hmac.new function and the hexdigest method
+    h = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
+
+    last_two_bytes = h[len(h) - 2 : len(h)]
+    # str to bytes
+    byte_val = bytes(last_two_bytes, "utf-8")
+    # bytes to int
+    int_val = int.from_bytes(byte_val, "big")
+
+    # print the output
+    return int_val % 100
+
+
 class TestStandardTransaction:
     @pytest_asyncio.fixture(scope="function")
     async def setup(self):
@@ -22,30 +49,48 @@ class TestStandardTransaction:
         await network.farm_block()
         yield network, alice, bob
 
-    async def make_and_spend_piggybank(self, network, alice, bob, CONTRIBUTION_AMOUNT,success_number, withdraw_amount,vault_after_withdraw_amount,vault_after_deposit_amount):#創建並存錢
+    async def make_and_spend_piggybank(
+        self,
+        network,
+        alice,
+        bob,
+        contribution_amount,
+        success_number,
+        withdraw_amount,
+        vault_after_withdraw_amount,
+        vault_after_deposit_amount,
+    ): 
         # Get our alice wallet some money
-        await network.farm_block(farmer=alice)#給alice 存錢
+        await network.farm_block(farmer=alice)  
 
-        # This will use one mojo to create our piggybank on the blockchain. 
-        piggybank_coin = await alice.launch_smart_coin (create_piggybank_puzzle(bob.puzzle_hash,50,10000000))#創建存錢罐,存滿會打到bob.puzzle_hash
-        # This retrieves us a coin that is at least 500 mojos.
-        contribution_coin = await alice.choose_coin(CONTRIBUTION_AMOUNT)#從alice 錢包內劃分出要放進去的token
+        # This will use one mojo to create our piggybank on the blockchain.
+        piggybank_coin = await alice.launch_smart_coin(
+            create_piggybank_puzzle(bob.puzzle_hash, PROBABILITY, GAME_LIMIT)
+        )  # 創建存錢罐,存滿會打到bob.puzzle_hash
 
-        #This is the spend of the piggy bank coin.  We use the driver code to create the solution.
+        contribution_coin = await alice.choose_coin(contribution_amount) 
+
+        # This is the spend of the piggy bank coin.  We use the driver code to create the solution.
         piggybank_spend = await alice.spend_coin(
             piggybank_coin,
             pushtx=False,
             # solution_for_piggybank(success_number,pb_coin, withdraw_amount,vault_after_withdraw_amount,vault_after_deposit_amount):
-            args=solution_for_piggybank(success_number,piggybank_coin.coin ,withdraw_amount,vault_after_withdraw_amount,vault_after_deposit_amount),
+            args=solution_for_piggybank(
+                success_number,
+                piggybank_coin.coin,
+                withdraw_amount,
+                vault_after_withdraw_amount,
+                vault_after_deposit_amount,
+            ),
         )
 
-        contribution_spend = await alice.spend_coin(#從alice 錢包扣錢
+        contribution_spend = await alice.spend_coin(  
             contribution_coin,
             pushtx=False,
         )
 
         # Aggregate them to make sure they are spent together
-        combined_spend = SpendBundle.aggregate([contribution_spend, piggybank_spend])#綁定在一起
+        combined_spend = SpendBundle.aggregate([contribution_spend, piggybank_spend]) 
 
         result = await network.push_tx(combined_spend)
         return result
@@ -54,15 +99,33 @@ class TestStandardTransaction:
     async def test_piggybank_withdraw(self, setup):
         network, alice, bob = setup
         try:
-            result = await self.make_and_spend_piggybank(network, alice, bob, 30,56,40,90,140)
+            contribution_amount = 30
+            success_number = 56  # larger then 50, so withdraw
+            withdraw_amount = contribution_amount * WITHDRAW_MULTIPLIER
+            vault_after_withdraw_amount = VAULT_BALANCE + contribution_amount - withdraw_amount
+            vault_after_deposit_amount = VAULT_BALANCE + contribution_amount
+            result = await self.make_and_spend_piggybank(
+                network,
+                alice,
+                bob,
+                contribution_amount,
+                success_number,
+                withdraw_amount,
+                vault_after_withdraw_amount,
+                vault_after_deposit_amount,
+            )
+            assert "error" not in result  # 不應該有err
 
-            assert "error" not in result #不應該有err
-
-            filtered_result = list(filter(
-                lambda addition:
-                    (addition.amount == 90) and
-                    (addition.puzzle_hash == create_piggybank_puzzle(bob.puzzle_hash,50,10000000).get_tree_hash())
-            ,result["additions"]))
+            filtered_result = list(
+                filter(
+                    lambda addition: (addition.amount == vault_after_withdraw_amount)
+                    and (
+                        addition.puzzle_hash
+                        == create_piggybank_puzzle(bob.puzzle_hash, PROBABILITY, GAME_LIMIT).get_tree_hash()
+                    ),
+                    result["additions"],
+                )
+            )
             assert len(filtered_result) == 1
         finally:
             await network.close()
@@ -71,28 +134,88 @@ class TestStandardTransaction:
     async def test_piggybank_deposit(self, setup):
         network, alice, bob = setup
         try:
-            result = await self.make_and_spend_piggybank(network, alice, bob, 30,46,40,90,140)
+            contribution_amount = 30
+            success_number = 46  # smaller then 50, so deposit
+            withdraw_amount = contribution_amount * WITHDRAW_MULTIPLIER
+            vault_after_withdraw_amount = VAULT_BALANCE + contribution_amount - withdraw_amount
+            vault_after_deposit_amount = VAULT_BALANCE + contribution_amount
+            result = await self.make_and_spend_piggybank(
+                network,
+                alice,
+                bob,
+                contribution_amount,
+                success_number,
+                withdraw_amount,
+                vault_after_withdraw_amount,
+                vault_after_deposit_amount,
+            )
 
             assert "error" not in result
 
-            filtered_result = list(filter(
-                lambda addition:
-                    (addition.amount == 140) and
-                    (addition.puzzle_hash == create_piggybank_puzzle(bob.puzzle_hash,50,10000000).get_tree_hash())
-            ,result["additions"]))
+            filtered_result = list(
+                filter(
+                    lambda addition: (addition.amount == vault_after_deposit_amount)
+                    and (
+                        addition.puzzle_hash
+                        == create_piggybank_puzzle(bob.puzzle_hash, PROBABILITY, GAME_LIMIT).get_tree_hash()
+                    ),
+                    result["additions"],
+                )
+            )
             assert len(filtered_result) == 1
         finally:
             await network.close()
 
-    # @pytest.mark.asyncio
-    # async def test_piggybank_stealing(self, setup):
-    #     network, alice, bob = setup
-    #     try:
-    #         result: Dict[str, List[Coin]] = await self.make_and_spend_piggybank(network, alice, bob, -100)
-    #         assert "error" in result
-    #         assert (
-    #             "GENERATOR_RUNTIME_ERROR" in result["error"]
-    #         )  # This fails during puzzle execution, not in driver code
-    #     finally:
-    #         await network.close()
+    @pytest.mark.asyncio
+    async def test_piggybank_secret_number(self, setup):
+        network, alice, bob = setup
+        try:
+            user_name = "Leo"
+            # secret_key =  # "20230202"
+            secret_key = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+            contribution_amount = 30
+            success_number = gen_success_number(user_name, secret_key)
+            withdraw_amount = contribution_amount * WITHDRAW_MULTIPLIER
+            vault_after_withdraw_amount = VAULT_BALANCE + contribution_amount - withdraw_amount
+            vault_after_deposit_amount = VAULT_BALANCE + contribution_amount
+
+            result = await self.make_and_spend_piggybank(
+                network,
+                alice,
+                bob,
+                contribution_amount,
+                success_number,
+                withdraw_amount,
+                vault_after_withdraw_amount,
+                vault_after_deposit_amount,
+            )
+
+            assert "error" not in result
+
+            if success_number > PROBABILITY:
+                filtered_result = list(
+                    filter(
+                        lambda addition: (addition.amount == vault_after_withdraw_amount)
+                        and (
+                            addition.puzzle_hash
+                            == create_piggybank_puzzle(bob.puzzle_hash, PROBABILITY, GAME_LIMIT).get_tree_hash()
+                        ),
+                        result["additions"],
+                    )
+                )
+                assert len(filtered_result) == 1
+            else:
+                filtered_result = list(
+                    filter(
+                        lambda addition: (addition.amount == vault_after_deposit_amount)
+                        and (
+                            addition.puzzle_hash
+                            == create_piggybank_puzzle(bob.puzzle_hash, PROBABILITY, GAME_LIMIT).get_tree_hash()
+                        ),
+                        result["additions"],
+                    )
+                )
+            assert len(filtered_result) == 1
+        finally:
+            await network.close()
 
